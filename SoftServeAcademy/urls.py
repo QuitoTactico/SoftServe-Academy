0336ca13ebad36f4c47236d228c776e25b6646f5
QuitoTactico <teteban0917@gmail.com>
2024-09-06 19:36:09 -0500

    first page

--- SoftServeAcademy/urls.py
+++ SoftServeAcademy/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', userViews.home, name='home'),
 ]
