15456d9b7635cfcd59d21ab5fb9e8e45e0c8cc75
QuitoTactico <teteban0917@gmail.com>
2024-09-07 02:13:07 -0500

    formularios autogenerados

--- preference/views.py
+++ preference/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
+from .models import Preference
 
-def create(request): 
-    return render(request, 'preference_create.html')
+class PreferenceForm(forms.ModelForm):
+    class Meta:
+        model = Preference
+        fields = ['mediaType', 'contentType', 'learningType', 'timePerWeek', 'timePerSession']
+        #widgets = {
+        #    'mediaType': forms.Select(attrs={'class': 'form-control'}),
+        #    'contentType': forms.Select(attrs={'class': 'form-control'}),
+        #    'learningType': forms.Select(attrs={'class': 'form-control'}),
+        #    'timePerWeek': forms.NumberInput(attrs={'class': 'form-control'}),
+        #    'timePerSession': forms.NumberInput(attrs={'class': 'form-control'}),
+        #}
+
+def create(request):
+    if request.method == 'POST':
+        form = PreferenceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user')  # Redirige a una URL de éxito
+    else:
+        form = PreferenceForm()
+    return render(request, 'preference_create.html', {'form': form})
 
 def detail(request, id: int):
     #id es el id del usuario. por ahora no se usa.
