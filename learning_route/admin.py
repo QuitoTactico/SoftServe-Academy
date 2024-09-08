12757f0a39deb78204f91046c922255bc0c4dca5
QuitoTactico <teteban0917@gmail.com>
2024-09-07 19:54:52 -0500

    learning route model

--- learning_route/admin.py
+++ learning_route/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import LearningRoute, LearningRouteResource
 
-# Register your models here.
+@admin.register(LearningRoute)
+class LearningRouteAdmin(admin.ModelAdmin):
+    list_display = ('skillLevel', 'duration', 'completed', 'time_spent')
+    list_filter = ('skillLevel', 'completed')
+    search_fields = ('skillLevel', 'duration', 'completed')
+
+@admin.register(LearningRouteResource)
+class LearningRouteResourceAdmin(admin.ModelAdmin):
+    list_display = ('LearningRoute', 'learning_resource', 'completed', 'time_spent')
+    list_filter = ('LearningRoute', 'completed')
+    search_fields = ('LearningRoute', 'learning_resource', 'completed')
+    ordering = ('LearningRoute', 'completed', 'time_spent')
--- learning_route/models.py
+++ learning_route/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from skill.models import SkillLevel
+from learning_resource.models import LearningResource
 
-# Create your models here.
+# id: int
+# LearningRoute: LearningRoute
+# learning_resource: LearningResource
+# completed: bool
+# time_spent: int
+class LearningRouteResource(models.Model):
+    LearningRoute = models.ForeignKey('LearningRoute', on_delete=models.CASCADE)
+    learning_resource = models.ForeignKey(LearningResource, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    time_spent = models.IntegerField(validators=[MinValueValidator(0)], blank=True)
+
+
+# id: int
+# skillLevel: SkillLevel
+# duration: int
+# learning_resources: learningRouteResource[]
+# actual_resource_index: int
+# completed: bool
+class LearningRoute(models.Model):
+    skillLevel = models.ForeignKey(SkillLevel, on_delete=models.CASCADE)
+    duration = models.IntegerField(validators=[MinValueValidator(1)])
+    learning_resources = models.ManyToManyField(LearningResource, through='LearningRouteResource')
+    actual_resource_index = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    time_spent = models.IntegerField(validators=[MinValueValidator(0)], blank=True)
+
+    def __str__(self):
+        return self.skillLevel.skill.name + ' - ' + str(self.skillLevel.level)
