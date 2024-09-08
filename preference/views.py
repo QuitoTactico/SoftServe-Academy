0d1830bba9eea4c7ee406681d861e74982e3cd6b
QuitoTactico <teteban0917@gmail.com>
2024-09-08 17:33:38 -0500

    forms format

--- preference/views.py
+++ preference/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render, redirect
-from django import forms
 from .models import Preference
-
-class PreferenceForm(forms.ModelForm):
-    class Meta:
-        model = Preference
-        fields = ['media_type', 'content_type', 'learning_type', 'time_per_week', 'time_per_session']
-        #widgets = {
-        #    'media_type': forms.Select(attrs={'class': 'form-control'}),
-        #    'content_type': forms.Select(attrs={'class': 'form-control'}),
-        #    'learning_type': forms.Select(attrs={'class': 'form-control'}),
-        #    'time_per_week': forms.NumberInput(attrs={'class': 'form-control'}),
-        #    'time_per_session': forms.NumberInput(attrs={'class': 'form-control'}),
-        #}
+from .forms import PreferenceForm
 
 def create(request):
     if request.method == 'POST':
