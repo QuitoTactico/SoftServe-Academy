a67a9513cf3f321094d65b9317ee115cccefba93
QuitoTactico <teteban0917@gmail.com>
2024-09-07 23:35:37 -0500

    route resource definitive fix

--- learning_route/admin.py
+++ learning_route/admin.py
@@ -1,20 +1,14 @@
 from django.contrib import admin
 from .models import LearningRoute, LearningRouteResource
 
-admin.site.register(LearningRoute)
-admin.site.register(LearningRouteResource)
-
-'''
 @admin.register(LearningRoute)
 class LearningRouteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'skill_level', 'duration', 'completed', 'time_spent')
-    list_filter = ('skill_level__skill__name', 'completed')
-    search_fields = ('id', 'skill_level', 'duration', 'completed')
+    list_display = ['id', 'skill_level', 'duration', 'completed', 'time_spent']
+    list_filter = ['skill_level__skill__name', 'completed']
+    search_fields = ['id', 'skill_level', 'duration', 'completed']
 
 @admin.register(LearningRouteResource)
 class LearningRouteResourceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'completed', 'time_spent')
-    list_filter = ('completed',)
-    search_fields = ('id', 'completed')
-    ordering = ('completed', 'time_spent')
-'''
+    list_display = ['id', 'learning_resource', 'completed', 'time_spent']
+    list_filter = ['completed', 'learning_resource__required_skills__skill__name']
+    search_fields = ['id', 'completed']
--- learning_route/models.py
+++ learning_route/models.py
@@ -8,9 +8,9 @@ from learning_resource.models import LearningResource
 # learning_resource: LearningResource
 # completed: bool
 # time_spent: int
+
 class LearningRouteResource(models.Model):
     id = models.AutoField(primary_key=True)
-    #Learning_route = models.ForeignKey('LearningRoute', on_delete=models.CASCADE)
     learning_resource = models.ForeignKey(LearningResource, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
     time_spent = models.IntegerField(validators=[MinValueValidator(0)], default=0)
@@ -22,18 +22,19 @@ class LearningRouteResource(models.Model):
 # id: int
 # skill_level: SkillLevel
 # duration: int
-# learning_resources: learningRouteResource[]
+# learning_route_resources: learningRouteResource[]
 # actual_resource_index: int
 # completed: bool
+# time_spent: int
+
 class LearningRoute(models.Model):
     id = models.AutoField(primary_key=True)
     skill_level = models.ForeignKey(SkillLevel, on_delete=models.CASCADE)
     duration = models.IntegerField(validators=[MinValueValidator(1)])
-    #learning_resources = models.ManyToManyField(LearningResource, through='LearningRouteResource')
-    learning_route_resources = models.ManyToManyField(LearningRouteResource, related_name='learning_route_resources')
+    learning_route_resources = models.ManyToManyField(LearningRouteResource, related_name='learning_routes', blank=True)
     actual_resource_index = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
-    time_spent = models.IntegerField(validators=[MinValueValidator(0)], blank=True)
+    time_spent = models.IntegerField(validators=[MinValueValidator(0)], default=0)
 
     def __str__(self):
         return self.skill_level.skill.name + ' - ' + str(self.skill_level.level)
