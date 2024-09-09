179db92dfd81a473b1dbde86474156d5184fe33e
MiguelCock <macockc@eafit.edu.co>
2024-09-09 08:52:02 -0500

    add signup test

---
+++ testsE2E/tests/user.spec.ts
@@ -0,0 +1,21 @@
+import { test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/');
+});
+
+test.describe('User tests', () => {
+
+  test('signup', async ({ page }) => {
+    await page.getByRole('link', { name: 'Get Started' }).click();
+    await page.getByLabel('Name:').click();
+    await page.getByLabel('Name:').fill('user1');
+    await page.getByLabel('Email:').click();
+    await page.getByLabel('Email:').fill('user1@email.com');
+    await page.getByLabel('Password:', { exact: true }).click();
+    await page.getByLabel('Password:', { exact: true }).fill('12345');
+    await page.getByLabel('Confirm password:').click();
+    await page.getByLabel('Confirm password:').fill('12345');
+    await page.getByRole('button', { name: 'Register' }).click();
+  });
+});
