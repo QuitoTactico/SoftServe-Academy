574945ec82e6a9370b1c5196160f877e3b438ee9
MiguelCock <macockc@eafit.edu.co>
2024-09-09 08:54:15 -0500

    added admin resource creation

--- testsE2E/tests/admin.spec.ts
+++ testsE2E/tests/admin.spec.ts
@@ -23,4 +23,43 @@ test.describe('Admin tests', () => {
     await page.getByLabel('Name:').fill('somethingX');
     await page.getByRole('button', { name: 'Save', exact: true }).click();
   });
+
+  test('add resourse', async ({ page }) => {
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await page.getByRole('row', { name: 'Learning resources Add Change' }).getByRole('link').nth(1).click();
+
+    await page.getByLabel('Name:').click();
+    await page.getByLabel('Name:').fill('resourseX');
+
+    const MEDIA_TYPES = ['Video', 'Audio', 'Text'];
+    await page.getByLabel('Media type:').selectOption(MEDIA_TYPES[Math.floor(Math.random() * MEDIA_TYPES.length)]);
+
+    const CONTENT_TYPES = ['Guides', 'Documentation', 'Introduction', 'Summary', 'Article', 'Quiz', 'Podcast'];
+    await page.getByLabel('Content type:').selectOption(CONTENT_TYPES[Math.floor(Math.random() * CONTENT_TYPES.length)]);
+
+    await page.getByLabel('Link:').click();
+    await page.getByLabel('Link:').fill('randomvideoX');
+
+    await page.getByLabel('Details:').click();
+    await page.getByLabel('Details:').fill('lorem ipsum');
+
+    await page.getByLabel('Duration:').click();
+    await page.getByLabel('Duration:').fill('123');
+
+    const LANGUAJES = ['en', 'es', 'ru', 'pt', 'zh', 'hi', 'ja']
+    await page.getByLabel('Language:').selectOption(LANGUAJES[Math.floor(Math.random() * LANGUAJES.length)]);
+
+    await page.getByLabel('Original platform:').click();
+    await page.getByLabel('Original platform:').fill('somewhere');
+
+    await page.getByLabel('Original author:').click();
+    await page.getByLabel('Original author:').fill('someone');
+
+    await page.getByLabel('General level:').click();
+    await page.getByLabel('General level:').fill(Math.ceil(1 + Math.random() * 4).toString());
+
+    await page.getByLabel('Learning skills:').selectOption(Math.floor(1 + Math.random() * 10).toString());
+    await page.getByLabel('Required skills:').selectOption(Math.floor(1 + Math.random() * 10).toString());
+    await page.getByRole('button', { name: 'Save', exact: true }).click();
+  });
 });
