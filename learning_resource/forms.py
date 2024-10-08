d4f5670575e92b32fb01565736c37332c0be4f78
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-10-08 12:58:05 -0500

    first resource form basic styling

--- learning_resource/forms.py
+++ learning_resource/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import LearningResource, Review
 
-
 class LearningResourceForm(forms.ModelForm):
     class Meta:
         model = LearningResource
@@ -19,24 +18,20 @@ class LearningResourceForm(forms.ModelForm):
             "learning_skills",
             "required_skills",
         ]
-        # fields = ['name', 'media_type', 'content_type', 'link', 'details', 'duration', 'language', 'original_platform', 'original_author', 'content_manager', 'general_level', 'learning_skills', 'required_skills', 'reviews']
         widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "media_type": forms.Select(attrs={"class": "form-control"}),
-            "content_type": forms.Select(attrs={"class": "form-control"}),
-            "link": forms.TextInput(attrs={"class": "form-control"}),
-            "details": forms.Textarea(attrs={"class": "form-control"}),
-            "duration": forms.NumberInput(attrs={"class": "form-control"}),
-            "language": forms.Select(attrs={"class": "form-control"}),
-            "original_platform": forms.TextInput(attrs={"class": "form-control"}),
-            "original_author": forms.TextInput(attrs={"class": "form-control"}),
-            #   'content_manager': forms.Select(attrs={'class': 'form-control'}),
-            "general_level": forms.NumberInput(attrs={"class": "form-control"}),
-            "learning_skills": forms.SelectMultiple(attrs={"class": "form-control"}),
-            "required_skills": forms.SelectMultiple(attrs={"class": "form-control"}),
-            #   'reviews': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            "name": forms.TextInput(attrs={"class": "custom-input"}),
+            "media_type": forms.Select(attrs={"class": "custom-select"}),
+            "content_type": forms.Select(attrs={"class": "custom-select"}),
+            "link": forms.TextInput(attrs={"class": "custom-input"}),
+            "details": forms.Textarea(attrs={"class": "custom-textarea"}),
+            "duration": forms.NumberInput(attrs={"class": "custom-input"}),
+            "language": forms.Select(attrs={"class": "custom-select"}),
+            "original_platform": forms.TextInput(attrs={"class": "custom-input"}),
+            "original_author": forms.TextInput(attrs={"class": "custom-input"}),
+            "general_level": forms.NumberInput(attrs={"class": "custom-input"}),
+            "learning_skills": forms.SelectMultiple(attrs={"class": "custom-select-multiple"}),
+            "required_skills": forms.SelectMultiple(attrs={"class": "custom-select-multiple"}),
         }
-
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -45,13 +40,13 @@ class ReviewForm(forms.ModelForm):
         widgets = {
             "comment": forms.Textarea(
                 attrs={
-                    "class": "form-control",
+                    "class": "form-control custom-textarea",
                     "placeholder": "Type Something...",
                     "required": True,
                 }
             ),
             "rate": forms.NumberInput(
-                attrs={"class": "form-control", "min": 1, "max": 5, "required": True}
+                attrs={"class": "form-control custom-input", "min": 1, "max": 5, "required": True}
             ),
         }
         labels = {
