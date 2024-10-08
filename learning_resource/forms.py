88b085713b9147479dfad6716ea184960a6b51fb
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-10-08 15:07:59 -0500

    learning_resource creation styling finished

--- learning_resource/forms.py
+++ learning_resource/forms.py
@@ -22,15 +22,15 @@ class LearningResourceForm(forms.ModelForm):
             "name": forms.TextInput(attrs={"class": "custom-input"}),
             "media_type": forms.Select(attrs={"class": "custom-select"}),
             "content_type": forms.Select(attrs={"class": "custom-select"}),
-            "link": forms.TextInput(attrs={"class": "custom-input"}),
+            "link": forms.URLInput(attrs={"class": "custom-input"}),
             "details": forms.Textarea(attrs={"class": "custom-textarea"}),
             "duration": forms.NumberInput(attrs={"class": "custom-input"}),
             "language": forms.Select(attrs={"class": "custom-select"}),
             "original_platform": forms.TextInput(attrs={"class": "custom-input"}),
             "original_author": forms.TextInput(attrs={"class": "custom-input"}),
             "general_level": forms.NumberInput(attrs={"class": "custom-input"}),
-            "required_skills": forms.SelectMultiple(attrs={"class": "form-control", "id": "id_required_skills"}),
-            "learning_skills": forms.SelectMultiple(attrs={"class": "form-control", "id": "id_learning_skills"}),
+            "required_skills": forms.SelectMultiple(attrs={"class": "form-control custom-select-multiple", "id": "id_required_skills"}),
+            "learning_skills": forms.SelectMultiple(attrs={"class": "form-control custom-select-multiple", "id": "id_learning_skills"}),
         }
 
 class ReviewForm(forms.ModelForm):
