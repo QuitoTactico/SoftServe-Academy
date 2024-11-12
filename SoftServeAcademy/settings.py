24c0744a7ec1e77bea4863337f7ab596f1dd7d64
QuitoTactico <teteban0917@gmail.com>
2024-11-11 23:04:51 -0500

    new requirements for autogenerating diagram

--- SoftServeAcademy/settings.py
+++ SoftServeAcademy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "django_extensions",
     "admin_interface",
     "colorfield",  # Required dependency for theming
     "django.contrib.admin",
